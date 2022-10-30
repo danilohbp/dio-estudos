acb7c6ef79d2f423643325c06e4bdbd2a72cceee
danilohbp <danil34681045@gmail.com>
2022-10-30 07:30:34 -0300

    Estudo sobre maven

---
+++ Gerenciamento de Dependências e Build em Java com Maven/component/src/main/java/com/mavencurso/Funcionalidade.java
@@ -0,0 +1,13 @@
+
+package com.mavencurso;
+
+/**
+ *
+ * @author danilo
+ */
+public class Funcionalidade {
+
+   public void msgHello() {
+       System.out.println("Hello World");
+   }
+}
---
+++ Gerenciamento de Dependências e Build em Java com Maven/quick-start-maven/src/main/java/com/mavencurso/App.java
@@ -0,0 +1,14 @@
+package com.mavencurso;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Funcionalidade f = new Funcionalidade();
+        f.msgHello();
+    }
+}
